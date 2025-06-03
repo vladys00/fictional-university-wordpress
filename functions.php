28cd6662f6492c03c9e032fe9cee73e7542a8464
Vladys K <vldk0022@gmail.com>
2025-06-03 23:24:19 +0200

    added dynamic menu

--- functions.php
+++ functions.php
@@ -13,6 +13,9 @@ add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features() {
     add_theme_support('title-tag');
+    register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    register_nav_menu('footerMenuOne', 'Footer Menu One');
+    register_nav_menu('footerMenuTwo', 'Footer Menu Two');
 }
 
 add_action('after_setup_theme', 'university_features')
