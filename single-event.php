a06ef6c578df261f820b7faf9a21bac721891b93
Vladys K <vldk0022@gmail.com>
2025-06-05 13:41:00 +0200

    updated permalink and display single and all events

--- single-event.php
+++ single-event.php
@@ -1,1 +1,32 @@
-test 123
+<?php
+
+     get_header();?>
+     <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title(); ?></h1>
+            <div class="page-banner__intro">
+                <p>DONT FORGET TO REPLACE ME LATER</p>
+            </div>
+        </div>
+    </div>
+    <?php while(have_posts()){
+        the_post(); ?>
+        <div class="container container--narrow page-section">
+             <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php echo site_url('/event'); ?>">
+                        <i class="fa fa-home" aria-hidden="true"></i> 
+                        Events Home
+                    </a> 
+                    <span class="metabox__main"><?php the_title() ?></span>
+                </p>
+            </div>
+            <!-- insert the content of the post down here  -->
+            <p><?php the_content() ;?></p>
+        </div>
+        <?php }
+    
+     get_footer();
+
+?>
