7730a9617db601936dae23a6c52ffec593251582
Vladys K <vldk0022@gmail.com>
2025-06-05 17:07:58 +0200

    displaying and ordering custom events by custom field value

--- front-page.php
+++ front-page.php
@@ -15,17 +15,34 @@
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
           <?php 
+            $today = date('Ymd');
             $homePageEvents = new WP_Query(array(
-              'posts_per_page'=>2,
+              'posts_per_page'=>-1,
               'post_type'=>'event',
+              'meta_key'=>'event_date',
+              'order_by'=>'meta_value_num',
+              'order'=>'ASC',
+              'meta_query'=>array(
+                array(
+                  'key'=>'event_date',
+                  'compare'=>'>=',
+                  'value'=>$today,
+                  'type'=>'numeric'
+                )
+              )
             ));
 
             while ($homePageEvents->have_posts()){
               $homePageEvents->the_post(); ?>
                   <div class="event-summary">
             <a class="event-summary__date t-center" href="<?php echo get_permalink() ?>">
-              <span class="event-summary__month"><?php echo get_the_time('M'); ?></span>
-              <span class="event-summary__day"><?php echo get_the_time('d'); ?></span>
+              <span class="event-summary__month"><?php 
+              $eventDate = new DateTime(get_field('event_date'));
+              echo $eventDate->format('M'); 
+              ?></span>
+              <span class="event-summary__day"><?php 
+              echo $eventDate->format('d'); 
+              ?></span>
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php echo get_permalink() ?>"><?php the_title(); ?></a></h5>
