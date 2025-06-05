8872133504539752c49bf9feead333fb3c8baecf
Vladys K <vldk0022@gmail.com>
2025-06-05 12:55:06 +0200

    added custom events post types

--- functions.php
+++ functions.php
@@ -15,6 +15,10 @@ function university_features() {
     add_theme_support('title-tag');
 }
 
-add_action('after_setup_theme', 'university_features')
+add_action('after_setup_theme', 'university_features');
+
+
 ?>
+
+
 
