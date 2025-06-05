01611dd727ce7c94a7ca005d87c6fd6d4909dbc1
Vladys K <vldk0022@gmail.com>
2025-06-05 18:21:35 +0200

    did pagination for custom query in past events

--- functions.php
+++ functions.php
@@ -17,6 +17,26 @@ function university_features() {
 
 add_action('after_setup_theme', 'university_features');
 
+function adjust_events_query($query) {
+    
+    if(!is_admin() && is_post_type_archive('event') && $query->is_main_query()){
+        $today = date('Ymd');
+        $query->set('meta_key','event_date');
+        $query->set('orderby','meta_value_num');
+        $query->set('order','ASC');
+        $query->set('meta_query', array(
+            array(
+                'key'=>'event_date',
+                'compare'=>'>=',
+                'value'=>$today,
+                'type'=>'numeric'
+            )
+        ));
+    }
+}
+
+add_action('pre_get_posts','adjust_events_query');
+
 
 ?>
 
