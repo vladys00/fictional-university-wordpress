7a9f1eda4863bcc2514c97b9b41365500ce5b761
Vladys K <vldk0022@gmail.com>
2025-06-10 20:06:20 +0200

    adde image

--- single-professor.php
+++ single-professor.php
@@ -2,7 +2,7 @@
 
      get_header();?>
      <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+        <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('banner_image'); echo $pageBannerImage ?>)"></div>
         <div class="page-banner__content container container--narrow">
             <h1 class="page-banner__title"><?php the_title(); ?></h1>
             <div class="page-banner__intro">
