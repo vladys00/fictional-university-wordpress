14ea7e29219289be0cb4a2312b18e5bb08fd0b9a
Vladys K <vldk0022@gmail.com>
2025-06-10 20:13:32 +0200

    added image to banner

--- single-professor.php
+++ single-professor.php
@@ -2,7 +2,8 @@
 
      get_header();?>
      <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('banner_image'); echo $pageBannerImage ?>)"></div>
+        <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('banner_image');
+         echo $pageBannerImage['sizes']['pageBanner'] ?>)"></div>
         <div class="page-banner__content container container--narrow">
             <h1 class="page-banner__title"><?php the_title(); ?></h1>
             <div class="page-banner__intro">
