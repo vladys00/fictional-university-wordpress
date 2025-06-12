395992934db8d5270eb970bb00b529b68987acfb
Vladys K <vldk0022@gmail.com>
2025-06-12 08:36:38 +0200

    adde single-campus and styles for it

---
+++ single-campus.php
@@ -0,0 +1,111 @@
+<?php
+
+     get_header();
+     pageBanner();?>
+     
+    <?php while(have_posts()){
+        the_post(); ?>
+        <div class="container container--narrow page-section">
+             <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('campus'); ?>">
+                        <i class="fa fa-home" aria-hidden="true"></i> 
+                        All campuses
+                    </a> 
+                    <span class="metabox__main"><?php the_title() ?></span>
+                </p>
+            </div>
+       
+            <div class="generic-content"><?php the_content() ;?></div>
+             <div class="acf-map">
+            <?php
+                $mapLocation = get_field('map_location')?>
+                <div class="marker" data-lat="<?php echo $mapLocation['lat'] ?>"
+                    data-lng="<?php echo $mapLocation['lng'] ?>">
+                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    <p>
+                        <?php echo $mapLocation['address']; ?>
+                    </p>
+                </div>
+            <?php  ?> 
+        </div>
+
+
+
+
+            <?php 
+            
+
+            $relatedProfessors = new WP_Query(array(
+              'posts_per_page'=>-1,
+              'post_type'=>'professor',
+              'order_by'=>'title',
+              'order'=>'DESC',
+              'meta_query'=>array(
+                array(
+                    'key'=>'related_programs',
+                    'compare'=>'LIKE',
+                    'value'=> '"'. get_the_ID(). '"',
+                )
+              )
+            ));
+            if ($relatedProfessors->have_posts()){
+                  echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">'. get_the_title() .' Professors</h2>';
+            echo '<ul class="professor-cards">';
+            while ($relatedProfessors->have_posts()){
+              $relatedProfessors->the_post(); ?>
+            <li class="professor-card__list-item">
+              <a class="professor-card" href="<?php the_permalink();  ?>">
+                <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>" alt="">
+                <span class="professor-card__name"><?php the_title(); ?></span>
+                
+              </a>
+            </li>
+          <?php }
+          echo '</ul>';
+            }
+
+            wp_reset_postdata();
+
+            $today = date('Ymd');
+            $homePageEvents = new WP_Query(array(
+              'posts_per_page'=>2,
+              'post_type'=>'event',
+              'meta_key'=>'event_date',
+              'order_by'=>'meta_value_num',
+              'order'=>'DESC',
+              'meta_query'=>array(
+                array(
+                  'key'=>'event_date',
+                  'compare'=>'>=',
+                  'value'=>$today,
+                  'type'=>'numeric'
+                ),
+                array(
+                    'key'=>'related_programs',
+                    'compare'=>'LIKE',
+                    'value'=> '"'. get_the_ID(). '"',
+                )
+              )
+            ));
+            if ($homePageEvents->have_posts()){
+                  echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium"> Upcoming '. get_the_title() .' Events</h2>';
+
+            while ($homePageEvents->have_posts()){
+              $homePageEvents->the_post();
+              get_template_part('template-parts/content-event');
+             }
+
+            }
+          
+          ?>
+        </div>
+
+            
+        <?php }
+    
+     get_footer();
+
+?>
