d689ccd2e8976d1386a47ab65165ef7961c7aee7
Vladys K <vldk0022@gmail.com>
2025-06-28 09:32:49 +0200

    added custom query wirth custom data coming from url

--- inc/search-route.php
+++ inc/search-route.php
@@ -11,21 +11,54 @@ add_action('rest_api_init','universityRegisterSearch');
 
 function universitySearchResults ($data) {
     $professors = new WP_Query(array(
-        'post_type' => 'professor',
+        'post_type' => array('professor', 'post' , 'page', 'program', 'event', 'campus'),
         's' => sanitize_text_field($data['term']),
     ));
-    $professorResults = array();
+    $results = array(
+        'generalInfo'=>array(),
+        'professors'=>array(),
+        'programs'=>array(),
+        'events'=>array(),
+        'campuses'=>array()   
+     );
 
     while ($professors->have_posts()) {
         $professors-> the_post();
-        array_push($professorResults, array(
+        if (get_post_type() == 'page' OR get_post_type() == 'post') {
+            array_push($results['generalInfo'], array(
             'title' => get_the_title(),
             'permalink' => get_permalink(),
         ));
+        }
+         if (get_post_type() == 'professor' ) {
+            array_push($results['professors'], array(
+            'title' => get_the_title(),
+            'permalink' => get_permalink(),
+        ));
+        }
+         if (get_post_type() == 'program') {
+            array_push($results['programs'], array(
+            'title' => get_the_title(),
+            'permalink' => get_permalink(),
+        ));
+        }
+         if (get_post_type() == 'event') {
+            array_push($results['events'], array(
+            'title' => get_the_title(),
+            'permalink' => get_permalink(),
+        ));
+        }
+         if (get_post_type() == 'campus') {
+            array_push($results['campuses'], array(
+            'title' => get_the_title(),
+            'permalink' => get_permalink(),
+        ));
+        }
+        
 
     };
 
-    return $professorResults;
+    return $results;
 };
 
 ?>
