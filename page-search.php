33a54114e9c5f8f0846d18ec1e797a2864e3af22
Vladys K <vldk0022@gmail.com>
2025-07-03 13:17:42 +0200

    added a serach page for non js

---
+++ page-search.php
@@ -0,0 +1,62 @@
+<?php
+get_header();
+
+while (have_posts()) {
+    the_post();
+    pageBanner(); ?>
+    
+   
+
+    <div class="container container--narrow page-section">
+        <?php
+        $theParent = wp_get_post_parent_id(get_the_ID());
+
+        if ($theParent) { ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>">
+                        <i class="fa fa-home" aria-hidden="true"></i> 
+                        Back to <?php echo get_the_title($theParent); ?>
+                    </a> 
+                    <span class="metabox__main"><?php the_title(); ?></span>
+                </p>
+            </div>
+        <?php }
+
+        $testArray = get_pages(array(
+            'child_of' => get_the_ID()
+        ));
+
+        if ($theParent || $testArray) { ?>
+            <div class="page-links">
+                <h2 class="page-links__title">
+                    <a href="<?php echo get_permalink($theParent); ?>">
+                        <?php echo get_the_title($theParent); ?>
+                    </a>
+                </h2>
+                <ul class="min-list">
+                    <?php
+                    $findChildrenOf = $theParent ? $theParent : get_the_ID();
+                    wp_list_pages(array(
+                        'title_li' => NULL,
+                        'child_of' => $findChildrenOf,
+                        'sort_column' => 'menu_order',
+                    ));
+                    ?>
+                </ul>
+            </div>
+        <?php } ?>
+
+        <div class="generic-content">
+            <form method="get" action="<?php echo esc_url(site_url('/')) ; ?>">
+                <input type="search" name="s">
+                <input type="submit" value="search">
+            </form>
+        </div>
+    </div>
+
+<?php 
+} // ← closing the while loop
+
+get_footer();
+?>
