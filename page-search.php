300519281b0059ad6478ba52a06f77ae90a96d8a
Vladys K <vldk0022@gmail.com>
2025-07-03 13:46:11 +0200

    done and styled non js search

--- page-search.php
+++ page-search.php
@@ -48,14 +48,7 @@ while (have_posts()) {
         <?php } ?>
 
         <div class="generic-content">
-            <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')) ; ?>">
-                <label class="headline headline--medium" for="s">Perform a new search</label>
-                
-                <div class="search-form-row">
-                    <input class="s" id="s" type="search" name="s" placeholder="what are yo looking for">
-                    <input class="search-submit" type="submit" value="search">
-                </div>
-            </form>
+            <?php get_search_form(); ?>
         </div>
     </div>
 
---
+++ searchform.php
@@ -0,0 +1,8 @@
+ <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')) ; ?>">
+                <label class="headline headline--medium" for="s">Perform a new search</label>
+                
+                <div class="search-form-row">
+                    <input class="s" id="s" type="search" name="s" placeholder="what are yo looking for">
+                    <input class="search-submit" type="submit" value="search">
+                </div>
+</form>
